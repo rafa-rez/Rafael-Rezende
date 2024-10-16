2fa16c143af4cc6765dd5e1eb73d71a3acca8f7c
rafael-rez <rafaelrez2004@gmail.com>
2024-10-16 15:50:49 -0300

    Criando classe D

---
+++ D.java
@@ -0,0 +1,17 @@
+public class D {
+    private int D1;
+    private float D2;
+    
+    public int MD1(){
+        System.out.print("MB1:");
+        return D1;
+    }
+
+    public float MD2(){
+        System.out.print("MB2:");
+        return  D2;
+    }
+    public void MD3(){
+        System.out.print("MD3");
+    }
+}
