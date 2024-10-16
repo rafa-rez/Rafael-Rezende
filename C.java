5b1aaa256eaf7da9024a551e15a0bde0e3c08813
rafael-rez <rafaelrez2004@gmail.com>
2024-10-16 15:22:28 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,14 @@
+public class C {
+    private int C1;
+    private float C2;
+    
+    public int MC1(){
+        System.out.print("MC1:");
+        return C1;
+    }
+
+    public float MC2(){
+        System.out.print("MC2:");
+        return  C2;
+    }
+}
