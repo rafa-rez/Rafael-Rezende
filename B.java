0ea735b631259635f049024e17e585f0bd90a13d
rafael-rez <rafaelrez2004@gmail.com>
2024-10-16 14:53:39 -0300

    :sparkles: adicionando classes iniciais.

---
+++ B.java
@@ -0,0 +1,14 @@
+public class B {
+    private int B1;
+    private float B2;
+    
+    public int MB1(){
+        System.out.print("MB1:");
+        return B1;
+    }
+
+    public float MB2(){
+        System.out.print("MB2:");
+        return  B2;
+    }
+}
